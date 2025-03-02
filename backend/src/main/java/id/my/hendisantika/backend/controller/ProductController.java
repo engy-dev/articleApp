be538bdc07fde50cd9842a14da37d6d6f1769e13
Engy Essam <153827798+engy-dev@users.noreply.github.com>
2025-03-02 12:00:57 +0100

    configuring/fixing errors with backend

---
+++ backend/src/main/java/id/my/hendisantika/backend/controller/ProductController.java
@@ -0,0 +1,61 @@
+package id.my.hendisantika.backend.controller;
+
+import id.my.hendisantika.backend.model.Product;
+import id.my.hendisantika.backend.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/19/24
+ * Time: 06:12
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateById(id, product);
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+}
---
+++ backend/src/main/java/id/my/hendisantika/backend/service/ProductService.java
@@ -0,0 +1,53 @@
+package id.my.hendisantika.backend.service;
+
+import id.my.hendisantika.backend.model.Product;
+import id.my.hendisantika.backend.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/19/24
+ * Time: 06:06
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateById(Long id, Product product) {
+        Product managedProduct = this.findById(id);
+        managedProduct.setTitle(product.getTitle());
+        managedProduct.setQuantity(product.getQuantity());
+        managedProduct.setPrice(product.getPrice());
+
+        return this.save(managedProduct);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found"));
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
